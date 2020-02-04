da5945d3782b42e9a88a785c1f37a07da0f55a8a
Jon Belt <jmb1303@live.co.uk>
2020-02-04 15:06:31 +0000

    commit

--- run.py
+++ run.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
+@app.route("/home")     #defines the HTML loaded for /home
 def home():
     return render_template('home.html', title='Home')
 
 
-@app.route("/login")
+@app.route("/login")    #defines the HTML loaded for /login
 def about():
     return render_template('login.html', title='Login')
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':      #always sets debug to be true
     app.run(debug=True)

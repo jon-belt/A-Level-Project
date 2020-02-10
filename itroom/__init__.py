bef81f0df7d96701c81597ced47727bf35634ee8
Jon Belt <jmb1303@live.co.uk>
2020-02-10 21:08:56 +0000

    commit

--- itroom/__init__.py
+++ itroom/__init__.py
@@ -9,4 +9,4 @@ app.config['SECRET_KEY'] = 'd0f59ff548e889290c6bf21f05d52afc'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
-from itroom.routes import routes
+from itroom import routes

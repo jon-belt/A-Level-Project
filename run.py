e670d90d5aa5e7b80f452c05db561cc0e102e116
Jon Belt <jmb1303@live.co.uk>
2020-02-10 19:39:15 +0000

    flask_migrate works

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import LoginForm
 from flask_migrate import Migrate
+from models import User, Post
 
 
 app = Flask(__name__)
@@ -10,32 +10,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd0f59ff548e889290c6bf21f05d52afc'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
-
-########################################################
-###                 Database Classes                 ###
-########################################################
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userEmail = db.Column(db.String(120), unique=True, nullable=False)
-    userPassword = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-
-    def __repr__(self):
-        return f"User('User')"
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"Post('{self.title}', '{self.date_posted}')"
-
 
 ###################################################
 ###                 Page Routes                 ###
